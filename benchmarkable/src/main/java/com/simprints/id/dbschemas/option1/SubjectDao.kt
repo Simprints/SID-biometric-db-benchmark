de0b50c108c7f9bfa19c75210556cee626084b6d
ethiery <etienne@simprints.com>
2025-04-24 09:58:04 +0100

    CR - part 1

--- benchmarkable/src/main/java/com/simprints/id/dbschemas/option1/SubjectDao.kt
+++ benchmarkable/src/main/java/com/simprints/id/dbschemas/option1/SubjectDao.kt
@@ -14,6 +14,14 @@ interface SubjectDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
      fun insertTemplates(templates: List<BiometricTemplate>)
 
+     // I don't think this is behaving the way you expect it to.
+     // If you build and go take a look at com/simprints/id/dbschemas/option1/SubjectDao_Impl.kt,
+     // you'll see that because you are using a @Relation annotation in SubjectWithTemplates, Room:
+     // - First performs the main query that joins with BiometricTemplate
+     // - But then ignore the BiometricTemplate.* colums, and runs one or more queries like
+     //  "SELECT `templateId`,`subjectId`,`format`,`fingerPosition`,`templateData` FROM `BiometricTemplate` WHERE `subjectId` IN (xxx)"
+     // This is not optimal performance wise. For a fair benchmark, I would stop using @Relation
+     // in SubjectWithTemplates so that only one SQL query is performed
     @Transaction
     @Query("""
         SELECT Subject.subjectId,BiometricTemplate.*  FROM Subject
